49e3092167c752a398bf0808422760bcacada0ed
Joe <biochemputer@gmail.com>
2016-06-24 22:30:32 -0400

    removed several lines

--- remove ACS PDFPlus.js
+++ remove ACS PDFPlus.js
@@ -9,12 +9,7 @@
 // ==/UserScript==
 
 (function() {
-// Get the element by their class name
-//var cur_columns = document.getElementsByClassName('icon-item icon-item-24px subordinate pdf-low-res');
 
-// Now remove them
-
-//for (var i = 0; i < cur_columns.length; i++) {
 $("li").remove(".pdf-low-res");
 
 })();
