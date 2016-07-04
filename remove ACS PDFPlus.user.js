5f7bcf6c4a40f22daec6ff6aef11ef0f61f33953
biochemputer <biochemputer@gmail.com>
2016-07-04 14:33:43 -0400

    Update remove ACS PDFPlus.user.js

--- remove ACS PDFPlus.user.js
+++ remove ACS PDFPlus.user.js
@@ -3,8 +3,6 @@
 // @namespace    http://example.com
 // @description  Ease Zotero Chrome Connector
 // @author       @ronery
-// @match        http://pubs.acs.org/*
-// @match        http://pubs.acs.org.*/*
 // @updateURL    https://github.com/biochemputer/Javascripts/raw/master/remove%20ACS%20PDFPlus.user.js
 // ==/UserScript==
 
